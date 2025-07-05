1e4af406ee130bbe1b0f51296583f9f1330d7b54
Haritha <98036195+haritha7592@users.noreply.github.com>
2025-07-05 21:31:51 +0530

    Update xpaths.py

--- Xpaths/xpaths.py
+++ Xpaths/xpaths.py
@@ -12,14 +12,14 @@ time.sleep(3)
 # driver.find_element(By.ID, "twotabsearchtextbox").send_keys("mobiles")
 # #driver.find_element(By.XPATH, "//a[text()='Mobiles']").click()
 # #wait = driver.implicitly_wait(5)
-# driver.implicitly_wait(3)
-# driver.find_element(By.ID, "nav-search-submit-button").click()
-# value= driver.find_element(By.XPATH, "//*[@class='a-color-state a-text-bold']")
-# status = value.text.strip('"')
-# print(status)
-# # print(f"Captured text: '{status}'")
-# #assert status.text is not None
-# assert status == "mobiles"
+driver.implicitly_wait(3)
+driver.find_element(By.ID, "nav-search-submit-button").click()
+value= driver.find_element(By.XPATH, "//*[@class='a-color-state a-text-bold']")
+status = value.text.strip('"')
+print(status)
+# print(f"Captured text: '{status}'")
+#assert status.text is not None
+assert status == "mobiles"
 
 driver.find_element(By.XPATH, "(//*[@class='nav-flyout-button nav-icon nav-arrow'])[2]")
 # driver.find_element(By.XPATH, "(//*[@class='a-icon a-icon-previous-rounded'])[1]")
@@ -35,5 +35,5 @@ driver.find_element(By.XPATH, "(//*[@class='nav-flyout-button nav-icon nav-arrow
 # # #wait.until()
 # #
 # #
-# # # driver.close()
+driver.close()
 # # driver.quit()
